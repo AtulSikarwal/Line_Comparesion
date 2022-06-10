9f77d7be3c35d34e51fdcfb3e519510f35ec77b4
AtulSikarwal <atulsikarwal5.com>
2022-06-10 13:34:34 +0530

    The Code for Checking for Two Lines are Equal or not

---
+++ TwoLineEqual/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LineCompersion
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            LineCompersion result = new LineCompersion();
+            double Value1 = result.Line();
+            double Value2 = result.LineTwo();
+
+            if (Value1 == Value2)
+            {
+                Console.WriteLine("Line is Equal");
+            }
+          
+        }
+    }
+}
