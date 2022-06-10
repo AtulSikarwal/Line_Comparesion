74d743f7ae0c8b8f997319273d044d003d7f04b6
AtulSikarwal <atulsikarwal5.com>
2022-06-10 13:38:09 +0530

    The code for checking for lines are greter and lessthen

---
+++ LineGreterOrLessthen/LineCompersion.cs
@@ -0,0 +1,48 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineCompersion
+{
+    internal class LineCompersion
+    {
+        public double Line()
+        {
+            Console.WriteLine("Enter the Value for X1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Value for X2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Value for Y1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Value for Y2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            double result = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.WriteLine(result);
+            return result;
+
+
+        }
+
+        public double LineTwo()
+        {
+            Console.WriteLine("Enter the Coodinates of line Two");
+            Console.WriteLine("Enter the Value for X1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Value for X2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Value for Y1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Value for Y2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            double result2 = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.WriteLine(result2);
+            return result2;
+
+
+        }
+    }
+}
---
+++ LineGreterOrLessthen/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace LineCompersion
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            LineCompersion result = new LineCompersion();
+            double Value1 = result.Line();
+            double Value2 = result.LineTwo();
+
+            if (Value1 == Value2)
+            {
+                Console.WriteLine("Line is Equal");
+            }
+            else if (Value1 > Value2)
+            {
+                Console.WriteLine("LineOne  is Greater of LineTwo ");
+            }
+            else
+            {
+                Console.WriteLine("LineOne is LessThen of LineTwo ");
+            }
+        }
+    }
+}
